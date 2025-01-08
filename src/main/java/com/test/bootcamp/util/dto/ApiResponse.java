f6490c1b61258afcdd21ad7057d760b42efc5c81
jeonmoo <rjm0710@naver.com>
2025-01-08 23:15:59 +0900

    ADD: custom response 객체 추가

---
+++ src/main/java/com/test/bootcamp/util/dto/ApiResponse.java
@@ -0,0 +1,42 @@
+package com.test.bootcamp.util.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.ResponseEntity;
+
+@Getter
+public class ApiResponse<T> {
+    private final Boolean isSuccess;
+    private final Error error;
+    private final T result;
+
+    @Getter
+    private static class Error {
+        private final String code;
+        private final String message;
+
+        @Builder
+        public Error(String code, String message) {
+            this.code = code;
+            this.message = message;
+        }
+    }
+
+    public ApiResponse(Boolean isSuccess, String errorCode, String errorMessage, T result) {
+        this.isSuccess = isSuccess;
+        this.error = Error.builder()
+                .code(errorCode)
+                .message(errorMessage)
+                .build();
+        this.result = result;
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> success(T result) {
+        return ResponseEntity.ok(new ApiResponse<>(true, null, null, result));
+    }
+
+    public static <T> ResponseEntity<ApiResponse<T>> fail(T result) {
+        return ResponseEntity.ok(new ApiResponse<>(false, null, null, null));
+    }
+
+}
