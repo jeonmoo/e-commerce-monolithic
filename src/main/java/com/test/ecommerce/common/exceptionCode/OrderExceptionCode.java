f93b3f0b7a911010b7caa1606808d8c332a137a6
jeonmoo <rjm0710@naver.com>
2025-04-21 21:39:38 +0900

    refactor: 소문자 제거

--- src/main/java/com/test/ecommerce/common/exceptionCode/OrderExceptionCode.java
+++ src/main/java/com/test/ecommerce/common/exceptionCode/OrderExceptionCode.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum OrderExceptionCode implements ExceptionCode {
 
     NOT_FOUND_ORDER("NOT_FOUND_ORDER", "없는 주문 입니다."),
-    NOT_FOUND_ORDER_Item("NOT_FOUND_ORDER_Item", "없는 주문상세 입니다."),
+    NOT_FOUND_ORDER_ITEM("NOT_FOUND_ORDER_Item", "없는 주문상세 입니다."),
     NOT_PENDING("NOT_PENDING", "주문대기 상태가 아닙니다."),
     INVALID_REFUND_REQUEST("INVALID_REFUND_REQUEST", "잘못된 환불요청 입니다."),
     NOT_REFUNDABLE_ORDER("NOT_REFUNDABLE_ORDER", "환불 할 수 없는 주문 입니다."),
