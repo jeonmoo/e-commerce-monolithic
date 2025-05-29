4d3f1e26c6a2285da32fcd165c04820f2da586ed
jeonmoo <rjm0710@naver.com>
2025-05-29 20:44:21 +0900

    refactor: 어노테이션 위치 이동

--- src/main/java/com/test/ecommerce/config/RedisConfig.java
+++ src/main/java/com/test/ecommerce/config/RedisConfig.java
@@ -1,6 +1,7 @@
 package com.test.ecommerce.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,6 +10,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
+@EnableCaching
 @Configuration
 public class RedisConfig {
 
