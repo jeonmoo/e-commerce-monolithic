6c31701dc8957f8d500c6c7b941bd3b7c2005773
jeonmoo <rjm0710@naver.com>
2025-05-30 23:50:47 +0900

    refactor: 어노테이션 위치 이동

--- src/main/java/com/test/ecommerce/EcommerceApplication.java
+++ src/main/java/com/test/ecommerce/EcommerceApplication.java
@@ -2,10 +2,8 @@ package com.test.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
-@EnableCaching
 public class EcommerceApplication {
 
 	public static void main(String[] args) {
