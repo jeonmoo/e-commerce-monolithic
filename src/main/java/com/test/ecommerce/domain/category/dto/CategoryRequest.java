6c525dad74aebee16f13f7b4551389b48dc7934f
jeonmoo <rjm0710@naver.com>
2025-06-10 22:02:27 +0900

    refactor: 카테고리 RequestDto 빌더 추가

--- src/main/java/com/test/ecommerce/domain/category/dto/CategoryRequest.java
+++ src/main/java/com/test/ecommerce/domain/category/dto/CategoryRequest.java
@@ -1,12 +1,15 @@
 package com.test.ecommerce.domain.category.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CategoryRequest {
 
     private Long parentId;
     private Integer depth;
     private Integer sort;
     private String categoryName;
+    private Boolean isDelete;
 }
