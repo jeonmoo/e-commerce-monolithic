757c66dd9cb71895782e8a1c28de1d0ba76f279a
jeonmoo <rjm0710@naver.com>
2025-06-17 22:45:46 +0900

    refactor: 주문조회 N +1  수정

--- src/main/java/com/test/ecommerce/domain/order/repository/OrderRepository.java
+++ src/main/java/com/test/ecommerce/domain/order/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.test.ecommerce.domain.order.repository;
 
 import com.test.ecommerce.domain.order.entity.Order;
 import com.test.ecommerce.domain.order.enums.OrderStatus;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    @EntityGraph(attributePaths = {"orderItems"})
     List<Order> findByUserId(Long userId);
 
     List<Order> findByUserIdAndOrderStatus(Long userId, OrderStatus orderStatus);
