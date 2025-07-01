205e8302b63506ed3771286cae25ef20ae4ad068
jeonmoo <rjm0710@naver.com>
2025-07-01 20:25:50 +0900

    refactor: 회원 dto 직렬화 추가

--- src/main/java/com/test/ecommerce/domain/user/dto/UserResponse.java
+++ src/main/java/com/test/ecommerce/domain/user/dto/UserResponse.java
@@ -3,9 +3,11 @@ package com.test.ecommerce.domain.user.dto;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
 @Builder
-public class UserResponse {
+public class UserResponse implements Serializable {
 
     private Long id;
     private String userName;
