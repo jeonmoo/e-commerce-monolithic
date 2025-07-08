079f69c28a8ac0e154011a4b858c438608b77174
jeonmoo <rjm0710@naver.com>
2025-07-08 21:27:52 +0900

    docs: user 테이블 인덱스 추가

---
+++ src/main/resources/db/migration/V9__create_index_users.sql
@@ -0,0 +1,4 @@
+CREATE UNIQUE INDEX idx_users_email ON users(email);
+CREATE INDEX idx_users_user_name ON users(user_name);
+CREATE INDEX idx_users_user_role ON users(user_role);
+CREATE INDEX idx_users_created_at ON users(created_at);
