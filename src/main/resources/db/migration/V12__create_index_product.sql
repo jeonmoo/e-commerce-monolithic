b1af5053fc3ae2c5c42482a3f4a0d754d0eab075
jeonmoo <rjm0710@naver.com>
2025-07-22 21:28:33 +0900

    docs: Product 테이블 인덱스 추가

---
+++ src/main/resources/db/migration/V12__create_index_product.sql
@@ -0,0 +1,4 @@
+CREATE INDEX idx_product_category_id_created_at ON product(category_id, created_at);
+CREATE INDEX idx_product_product_name_created_at ON product(procudt_name, created_at);
+CREATE INDEX idx_product_final_price_created_at ON product(final_price, created_at);
+CREATE INDEX idx_product_created_at ON product(created_at);
