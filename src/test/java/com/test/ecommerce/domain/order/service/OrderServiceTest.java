29975e3bb249237811d719286761ce138206f37f
jeonmoo <rjm0710@naver.com>
2025-08-07 23:14:41 +0900

    test: Order 테스트 캐시 클리어 추가

--- src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
@@ -1,7 +1,28 @@
 package com.test.ecommerce.domain.order.service;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Transactional
+@ActiveProfiles("test")
+@SpringBootTest
 class OrderServiceTest {
 
+    @Autowired
+    RedisTemplate<String, String> redisTemplate;
+
+    @BeforeEach
+    void clear() {
+        // 메서드 실행 전 캐시 초기화
+        redisTemplate.getConnectionFactory()
+                .getConnection()
+                .serverCommands()
+                .flushAll();
+    }
 }
