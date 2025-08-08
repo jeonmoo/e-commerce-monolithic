af1b6faa682495bb26d5ee5d444049c897d72ea2
jeonmoo <rjm0710@naver.com>
2025-08-08 23:42:08 +0900

    test: Order 테스트 시작시 테스트 데이터 생성 추가

--- src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
@@ -1,13 +1,19 @@
 package com.test.ecommerce.domain.order.service;
 
+import com.test.ecommerce.domain.category.entity.Category;
+import com.test.ecommerce.domain.category.repository.CategoryRepository;
+import com.test.ecommerce.domain.product.entity.Product;
+import com.test.ecommerce.domain.product.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
 
 @Transactional
 @ActiveProfiles("test")
@@ -17,6 +23,12 @@ class OrderServiceTest {
     @Autowired
     RedisTemplate<String, String> redisTemplate;
 
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
     @BeforeEach
     void clear() {
         // 메서드 실행 전 캐시 초기화
@@ -24,5 +36,31 @@ class OrderServiceTest {
                 .getConnection()
                 .serverCommands()
                 .flushAll();
+    }
+
+    @BeforeEach
+    void setData() {
+        Category category = new Category();
+        category.setDepth(0);
+        category.setSort(0);
+        category.setCategoryName("Digital");
+        category.setIsDelete(false);
+        categoryRepository.save(category);
+
+        Product product = Product.builder()
+                .productName("Macbook M1 Air")
+                .category(category)
+                .quantity(100)
+                .originPrice(BigDecimal.valueOf(10000))
+                .discountPrice(BigDecimal.ZERO)
+                .finalPrice(BigDecimal.valueOf(10000))
+                .isDelete(false)
+                .build();
+    }
+
+    @Test
+    @DisplayName("주문등록 - 주문을 등록한다")
+    void registerOrderTest() {
+
     }
 }
