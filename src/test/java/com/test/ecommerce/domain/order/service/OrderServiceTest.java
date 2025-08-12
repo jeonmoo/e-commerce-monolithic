4840eaff38d24f1eedb1e27b25bf80ff29f06d5b
jeonmoo <rjm0710@naver.com>
2025-08-12 20:48:35 +0900

    test: 오더 테스트 before 버그 수정

--- src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
+++ src/test/java/com/test/ecommerce/domain/order/service/OrderServiceTest.java
@@ -4,13 +4,18 @@ import com.test.ecommerce.domain.category.entity.Category;
 import com.test.ecommerce.domain.category.repository.CategoryRepository;
 import com.test.ecommerce.domain.product.entity.Product;
 import com.test.ecommerce.domain.product.repository.ProductRepository;
+import com.test.ecommerce.domain.user.entity.User;
+import com.test.ecommerce.domain.user.enums.UserRole;
+import com.test.ecommerce.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
@@ -18,16 +23,20 @@ import java.math.BigDecimal;
 @Transactional
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 class OrderServiceTest {
 
     @Autowired
     RedisTemplate<String, String> redisTemplate;
-
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     CategoryRepository categoryRepository;
-
     @Autowired
     ProductRepository productRepository;
+
+    @Autowired
+    MockMvc mockMvc;
 
     @BeforeEach
     void clear() {
@@ -40,6 +49,15 @@ class OrderServiceTest {
 
     @BeforeEach
     void setData() {
+        User user = User.builder()
+                .userRole(UserRole.USER)
+                .userName("테스트 유저")
+                .password("test")
+                .phoneNumber("010-0000-0000")
+                .email("test@test.com")
+                .build();
+        userRepository.save(user);
+
         Category category = new Category();
         category.setDepth(0);
         category.setSort(0);
@@ -56,6 +74,7 @@ class OrderServiceTest {
                 .finalPrice(BigDecimal.valueOf(10000))
                 .isDelete(false)
                 .build();
+        productRepository.save(product);
     }
 
     @Test
