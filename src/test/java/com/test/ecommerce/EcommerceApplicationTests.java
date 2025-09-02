5de9caeb32664dcdafbcfcb229c795df649c4381
jeonmoo <rjm0710@naver.com>
2025-09-02 21:37:44 +0900

    test: 테스트 실패 수정

--- src/test/java/com/test/ecommerce/EcommerceApplicationTests.java
+++ src/test/java/com/test/ecommerce/EcommerceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.test.ecommerce;
 
+import com.test.ecommerce.config.TestContainerBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EcommerceApplicationTests {
+class EcommerceApplicationTests extends TestContainerBase {
 
 	@Test
 	void contextLoads() {
