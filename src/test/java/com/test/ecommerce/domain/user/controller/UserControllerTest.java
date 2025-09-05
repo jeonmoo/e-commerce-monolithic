9af681bc8365f0fef6d9b73b8328ba8b2adcad6f
jeonmoo <rjm0710@naver.com>
2025-09-05 23:56:33 +0900

    test: User 회원가입 api 테스트 추가

--- src/test/java/com/test/ecommerce/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/test/ecommerce/domain/user/controller/UserControllerTest.java
@@ -2,13 +2,21 @@ package com.test.ecommerce.domain.user.controller;
 
 import com.test.ecommerce.config.TestContainerBase;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @Transactional
 @ActiveProfiles("test")
@@ -30,9 +38,33 @@ class UserControllerTest extends TestContainerBase {
                 .flushAll();
     }
 
-    @BeforeEach
-    void setData() {
-        
+    @Test
+    @DisplayName("사용자 회원가입 - 사용자 회원가입을 한다.")
+    void createUserTest() throws Exception {
+        String userName = "테스트유저";
+        String password = "test1234";
+        String phoneNumber = "01000000000";
+        String email = "test@test.com";
+
+        String requestBody = """
+                {
+                  "userName": "%s",
+                  "password": "%s",
+                  "phoneNumber": "%s",
+                  "email": "%s"
+                }
+                """.formatted(userName, password, phoneNumber, email);
+
+        mockMvc.perform(post("/user")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestBody))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.isSuccess").value(true))
+                .andExpect(jsonPath("$.result.userName").value(userName))
+                .andExpect(jsonPath("$.result.password").value(password))
+                .andExpect(jsonPath("$.result.phoneNumber").value(phoneNumber))
+                .andExpect(jsonPath("$.result.email").value(email));
     }
 
 }
