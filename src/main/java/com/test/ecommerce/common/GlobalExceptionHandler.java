a33e6ccb60f08b9b5f65fbf5d0291951a284d925
jeonmoo <rjm0710@naver.com>
2025-09-23 23:16:39 +0900

    feat: 에러 로그 추가

--- src/main/java/com/test/ecommerce/common/GlobalExceptionHandler.java
+++ src/main/java/com/test/ecommerce/common/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.test.ecommerce.common;
 
 
 import io.swagger.v3.oas.annotations.Hidden;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,11 +11,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import java.util.concurrent.atomic.AtomicReference;
 
 @Hidden
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GlobalException.class)
     public ResponseEntity<?> userException(GlobalException e) {
+        log.error(e.getMessage(), e);
         return ApiResponse.fail(e.getCode(), e.getMessage());
     }
 
@@ -23,12 +26,14 @@ public class GlobalExceptionHandler {
         e.printStackTrace();
         AtomicReference<String> errors = new AtomicReference<>();
         e.getBindingResult().getAllErrors().forEach(error -> errors.set(error.getDefaultMessage()));
+        log.error(e.getMessage(), e);
         return ApiResponse.fail(e.getStatusCode().toString(), String.valueOf(errors));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> serverException(Exception e) {
         e.printStackTrace();
+        log.error(e.getMessage(), e);
         return ApiResponse.serverException("SERVER_ERROR", e.getMessage());
     }
 }
