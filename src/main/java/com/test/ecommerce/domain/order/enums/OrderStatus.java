c3ec2cf77f04972ce6e6e23f3fa35468d18ae371
jeonmoo <rjm0710@naver.com>
2025-11-01 13:14:38 +0900

    refactor: orderStatus 수정

--- src/main/java/com/test/ecommerce/domain/order/enums/OrderStatus.java
+++ src/main/java/com/test/ecommerce/domain/order/enums/OrderStatus.java
@@ -9,10 +9,10 @@ public enum OrderStatus {
     PENDING("PENDING", "주문대기"),
     COMPLETE("COMPLETE", "주문완료"),
     CANCELED("CANCELED", "주문취소"),
-    PARTIALLY_REQUIRED_REFUND("REQUIRED_REFUND", "부분환불요청"),
+    PARTIALLY_REQUIRED_REFUND("PARTIALLY_REQUIRED_REFUND", "부분환불요청"),
     REQUIRED_REFUND("REQUIRED_REFUND", "환불요청"),
     PARTIALLY_REFUNDED("PARTIALLY_REFUNDED", "부분환불"),
-    REFUNDED("REFUND", "환불"),
+    REFUNDED("REFUNDED", "환불"),
     ;
 
     private final String status;
