2950fb3004069f1e511baf59a70f011c16f8fea0
jeonmoo <rjm0710@naver.com>
2025-11-16 23:43:32 +0900

    refactor: 카테고리 response 구조 버그 수정

--- src/main/java/com/test/ecommerce/domain/category/dto/CategoryResponse.java
+++ src/main/java/com/test/ecommerce/domain/category/dto/CategoryResponse.java
@@ -2,11 +2,15 @@ package com.test.ecommerce.domain.category.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Setter
 @Builder
 public class CategoryResponse implements Serializable {
 
@@ -16,6 +20,7 @@ public class CategoryResponse implements Serializable {
     private Integer sort;
     private String categoryName;
     private Boolean isDelete;
+    List<CategoryResponse> subCategories = new ArrayList<>();
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 }
